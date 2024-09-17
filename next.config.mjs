5319c826db751e08ae11670d47b820411aabca8b
SantiagoDoval <danieldovals@gmail.com>
2024-09-17 00:15:44 -0500

    test:crypto

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,38 @@
 
 // import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-const nextConfig = {
+// const nextConfig = {
     
+// };
+
+// // if (process.env.NODE_ENV === 'development') {
+// //   await setupDevPlatform();
+// // }
+// export default nextConfig;
+
+const webpack = require('webpack');
+
+const nextConfig = {
+  webpack: (config) => {
+    // Añadir polyfill para `crypto`
+    config.resolve.fallback = {
+      crypto: require.resolve('crypto-browserify'),
+    };
+
+    // Añadir soporte para `process` y `Buffer`
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      })
+    );
+
+    return config;
+  },
 };
 
-// if (process.env.NODE_ENV === 'development') {
-//   await setupDevPlatform();
-// }
 export default nextConfig;
+
 
 
 
